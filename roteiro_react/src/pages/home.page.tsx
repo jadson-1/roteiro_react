634fe2e81ecd019b9b53bf8a55d2779397c49587
jadson-1 <63202719+jadson-1@users.noreply.github.com>
2024-10-21 23:26:11 +0000

    primeira parte

---
+++ roteiro_react/src/pages/home.page.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Profile from '../components/profile.component';
+import '../App.css';
+
+const HomePage: React.FC = () => {
+    return (
+        <div className="App">
+            <Profile
+                name="Katherine Johnson"
+                imageUrl="https://i.imgur.com/MK3eW3Am.jpg"
+            />
+            <Profile
+                name="Dorothy Vaughan"
+                imageUrl="https://i.imgur.com/7vQD0fPs.jpg"
+            />
+            <Profile
+                name="Mary Jackson"
+                imageUrl="https://i.imgur.com/Z9SnKhp.jpg"
+            />
+        </div>
+    );
+};
+
+export default HomePage;
