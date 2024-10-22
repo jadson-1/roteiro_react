88c41fb1284d72a08d09645610ce76c89d6e8dc7
jadson-1 <63202719+jadson-1@users.noreply.github.com>
2024-10-22 13:23:36 +0000

    roteiro parte 1.2

--- roteiro_react/src/App.tsx
+++ roteiro_react/src/App.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Logo from "./components/Logo";
 import Principal from "./components/Principal";
+import './App.css'; // Importando o arquivo de estilos
 
 const App = () => {
   return (
+    <div className="container">
+    <h1 className="title">Hello, World!</h1>
     <>
       <Logo />
       <Principal />
     </>
+    </div>
   );
 };
 
