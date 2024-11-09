64f6a1f75584764d15809d05b548ca14549ee5da
jadson-1 <63202719+jadson-1@users.noreply.github.com>
2024-11-09 12:35:45 +0000

    Formulario

--- roteiro_react/src/components/Principal.tsx
+++ roteiro_react/src/components/Principal.tsx
@@ -1,30 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
+import BookForm from './BookForm';
+import BookList from './BookList';
+import { Book } from '../interfaces/book.interface';
 
 const Principal = () => {
+    const [books, setBooks] = useState<Book[]>([]);
+
+    const addBook = (book: Book) => {
+        setBooks((prevBooks) => [...prevBooks, book]);
+    };
+
     return (
-        <main className="principal">
-            <h2>Últimos lançamentos</h2>
-            <div className="card">
-                <div className="thumb">
-                    <img src="imagens/capas/9788575228142.jpg" alt="" />
-                </div>
-                <div className="detalhes">
-                    <h3>Padrões para Kubernetes</h3>
-                    <p>O modo como os ...</p>
-                    <a href="#">Leia mais </a>
-                </div>
-                </div>
-            <div className="card">
-                <div className="thumb">
-                    <img src="imagens/capas/9788575228074.jpg" alt="" />
-                </div>
-                <div className="detalhes">
-                    <h3>Introdução ao Pentest – 2a Edição</h3>
-                    <p>Introdução ao Pentest...</p>
-                    <a href="#">Leia mais </a>
-                </div>
-            </div>
-        </main>
+        <div className="App">
+            <h1>Cadastro de Livros</h1>
+            <BookForm onAddBook={addBook} />
+            <BookList books={books} />
+        </div>
     );
 };
 
